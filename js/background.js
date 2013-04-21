eda2fdba43f6ffb6bc346aa8fe1076822959da12
Joshua Parrish <jparrish88@cox.net>
2013-04-20 23:46:07 -0700

    added files

---
+++ js/background.js
@@ -0,0 +1,11 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('../window.html', {
+    'bounds': {
+      'width': 1024,
+      'height': 768
+    },
+	minWidth: 600,
+    minHeight: 500,
+    frame: 'none'
+  });
+});
